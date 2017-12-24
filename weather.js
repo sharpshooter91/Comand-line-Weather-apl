2ae86f3d16c6e371f29f3726a82d04bd38b1b61e
sharpshooter91 <janko307@gmail.com>
2017-12-24 14:46:52 +0100

    Second commit
    Making changes...

--- weather.js
+++ weather.js
@@ -1,26 +1,26 @@
 const http= require("http");
-const query="nis";
-const api= require('./api.json');
+const api= require("./api.json");
+//const query="gadzin han";
 
-function printMessage(city, temperature , airPresure) {
-  const string= `In ${city} temperature is ${Math.round(temperature/17.2222)} and air resure is ${parseFloat(airPresure).toFixed( 2 )}`;
+function printMessage(weather) {
+  const string= `It is ${Math.round( weather.list[0].main.temp-272.15 )} degrees in ${weather.city.name}`;
   console.log(string);
 };
 
 function get(query) {
+
   const request= http.get(`http://api.openweathermap.org/data/2.5/forecast?q=${query}&APPID=${api.key}`, response=> {
     let body= "";
     response.on("data", data=>{
-      body+= data.toString();
-      //console.log(body);
+      body+= data;
+      //console.log(typeof body);
     });
-    response.on("end", ()=>{
-      const core= JSON.parse(body);
-      printMessage(query, core.list[0].main.temp, core.list[0].main.pressure);
+    response.on("end", ()=> {
+      let weather= JSON.parse(body);
+      printMessage(weather);
     });
   });
 };
 
-get(query);
-
 module.exports.get= get;
+//get(query);
